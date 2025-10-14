10056a61fa36d8ba390323703d9d78cb7ef186eb
Natnael <natimule2015@gmail.com>
2025-10-14 21:30:54 +0300

    Remove unnecessary Tradeas folder before Netlify deploy

--- Tradeas/src/pages/Challenges.jsx
+++
@@ -1,3 +0,0 @@
-export default function Challenges() {
-  return <h1 className="text-2xl font-bold">Challenges Page</h1>;
-}
--- Tradeas/src/pages/Notebook.jsx
+++
@@ -1,3 +0,0 @@
-export default function Notebook() {
-  return <h1 className="text-2xl font-bold">Notebook Page</h1>;
-}
