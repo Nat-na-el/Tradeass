79f9f525d8d0a8242f0f044e1cbde910a70236df
Natnael Mulugeta <131980126+Nat-na-el@users.noreply.github.com>
2026-02-03 19:12:46 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/tradeass/", // 👈 repo name EXACTLY
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
